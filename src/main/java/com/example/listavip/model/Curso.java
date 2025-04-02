73feec354dea92346775f4347965309b28e2415e
Gabiuji <ogabrieliuji@gmail.com>
2025-04-02 12:13:39 -0400

    Criação da arquitetura MVC

---
+++ src/main/java/com/example/listavip/model/Curso.java
@@ -0,0 +1,4 @@
+package com.example.listavip.model;
+
+public class Curso {
+}
--- src/main/java/com/example/listavip/view/MainActivity.java
+++ src/main/java/com/example/listavip/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.listavip;
+package com.example.listavip.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.listavip.R;
 
 public class MainActivity extends AppCompatActivity {
 
