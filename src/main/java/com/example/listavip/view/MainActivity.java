0ec61fa54bede1695d3731f79232608cdcec331a
Gabiuji <ogabrieliuji@gmail.com>
2025-04-14 14:56:53 -0400

    conf de Log

--- src/main/java/com/example/listavip/view/MainActivity.java
+++ src/main/java/com/example/listavip/view/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.listavip.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
 import com.example.listavip.R;
 import com.example.listavip.model.Pessoa;
@@ -11,12 +13,18 @@ public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
 
+    public EditText editPrimeiroNome;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+
+        pessoa.setPrimeiroNome();
+
+        Log.i("POO informações", pessoa.toString()); //É mostrado no Logcat quais informações foram inseridas na classe Pessoa
 
     }
 }
