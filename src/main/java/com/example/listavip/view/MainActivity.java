5d10a07508e4d5a62098fc717d38f76154b1334d
Gabiuji <ogabrieliuji@gmail.com>
2025-04-16 10:57:02 -0400

    salvando informações inseridas no editText

--- src/main/java/com/example/listavip/view/MainActivity.java
+++ src/main/java/com/example/listavip/view/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.listavip.R;
 import com.example.listavip.model.Pessoa;
@@ -13,7 +16,13 @@ public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
 
-    public EditText editPrimeiroNome;
+    EditText editPrimeiroNome;
+    EditText editSobrenome;
+    EditText editNomeCurso;
+    EditText editTelefone;
+    Button btnLimpar;
+    Button btnSalvar;
+    Button btnFinalizar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +31,44 @@ public class MainActivity extends AppCompatActivity {
 
         pessoa = new Pessoa();
 
-        pessoa.setPrimeiroNome();
+        editPrimeiroNome = findViewById(R.id.editTextName);
+        editSobrenome = findViewById(R.id.editTextSobre);
+        editNomeCurso = findViewById(R.id.editTextCurso);
+        editTelefone = findViewById(R.id.editTextTelefone);
+
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editPrimeiroNome.setText("");
+                editSobrenome.setText("");
+                editNomeCurso.setText("");
+                editTelefone.setText("");
+            }
+        });
+
+        btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Volte sempre", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pessoa.setPrimeiroNome(editPrimeiroNome.getText().toString());
+                pessoa.setSobrenome(editSobrenome.getText().toString());
+                pessoa.setCursoDesejado(editNomeCurso.getText().toString());
+                pessoa.setTelefone(editTelefone.getText().toString());
+
+                Toast.makeText(MainActivity.this, "Dados Salvos com Sucesso" + pessoa.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
 
         Log.i("POO informações", pessoa.toString()); //É mostrado no Logcat quais informações foram inseridas na classe Pessoa
 
